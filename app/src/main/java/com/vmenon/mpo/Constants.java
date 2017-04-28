d6fbde2084d63c869b5ecc3dd9849b5e9d9775b4
Vinayak Menon <vviswana@gmail.com>
2017-04-28 18:46:58 -0500

    Updating gradle Pointing to heroku app endpoint

--- app/src/main/java/com/vmenon/mpo/Constants.java
+++ app/src/main/java/com/vmenon/mpo/Constants.java
@@ -1,6 +1,5 @@
 package com.vmenon.mpo;
 
 public final class Constants {
-    //public final static String API_URL = "http://ec2-54-209-237-209.compute-1.amazonaws.com/";
-    public final static String API_URL = "http://10.0.0.211:8080/";
+    public final static String API_URL = "https://mpospboot.herokuapp.com/";
 }
