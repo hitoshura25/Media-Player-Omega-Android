15f0e5ff1a234c9ee75553eb1b162351331d5ca3
Vinayak Menon <vviswana@gmail.com>
2017-10-20 14:06:13 -0500

    Minor UI fixes

--- app/src/main/java/com/vmenon/mpo/core/MPOExoPlayer.java
+++ app/src/main/java/com/vmenon/mpo/core/MPOExoPlayer.java
@@ -208,7 +208,22 @@ public class MPOExoPlayer extends MPOPlayer {
 
         @Override
         public void onPlayerError(ExoPlaybackException error) {
+            final String what;
+            switch (error.type) {
+                case ExoPlaybackException.TYPE_SOURCE:
+                    what = error.getSourceException().getMessage();
+                    break;
+                case ExoPlaybackException.TYPE_RENDERER:
+                    what = error.getRendererException().getMessage();
+                    break;
+                case ExoPlaybackException.TYPE_UNEXPECTED:
+                    what = error.getUnexpectedException().getMessage();
+                    break;
+                default:
+                    what = "Unknown: " + error;
+            }
 
+            Log.w("MPO", "ExoPlayer error: what=" + what);
         }
 
         @Override
