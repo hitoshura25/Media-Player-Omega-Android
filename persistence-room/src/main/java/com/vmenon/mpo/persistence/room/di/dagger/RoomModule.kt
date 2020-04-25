c60eb20ffe76ddb1b53bb3c8fb34ebb367d8a8ba
Vinayak Menon <vviswana@gmail.com>
2020-04-24 21:02:31 -0500

    Ensuring RoomModule dependencies are singletons

--- persistence-room/src/main/java/com/vmenon/mpo/persistence/room/di/dagger/RoomModule.kt
+++ persistence-room/src/main/java/com/vmenon/mpo/persistence/room/di/dagger/RoomModule.kt
@@ -9,10 +9,12 @@ import com.vmenon.mpo.persistence.room.dao.ShowDao
 import com.vmenon.mpo.persistence.room.dao.ShowSearchResultDao
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class RoomModule {
     @Provides
+    @Singleton
     fun provideMPODatabase(application: Application): MPODatabase {
         return Room.databaseBuilder(
             application.applicationContext, MPODatabase::class.java,
@@ -21,21 +23,25 @@ class RoomModule {
     }
 
     @Provides
+    @Singleton
     fun provideShowDao(mpDatabase: MPODatabase): ShowDao {
         return mpDatabase.showDao()
     }
 
     @Provides
+    @Singleton
     fun provideEpisodeDao(mpDatabase: MPODatabase): EpisodeDao {
         return mpDatabase.episodeDao()
     }
 
     @Provides
+    @Singleton
     fun provideDownloadDao(mpDatabase: MPODatabase): DownloadDao {
         return mpDatabase.downloadDao()
     }
 
     @Provides
+    @Singleton
     fun provideShowSearchResultsDao(mpDatabase: MPODatabase): ShowSearchResultDao {
         return mpDatabase.showSearchResultsDao()
     }
