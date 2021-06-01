40da5acc2ba939981e56a60fa5701f435da5b0f3
Vinayak Menon <vviswana@gmail.com>
2021-06-01 18:11:31 -0500

    Fixing resource/drawable that were not in the "view" module

--- login-feature/src/androidTest/java/com/vmenon/mpo/login/ExampleInstrumentedTest.kt
+++ login-feature/src/androidTest/java/com/vmenon/mpo/login/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.vmenon.mpo.login", appContext.packageName)
+        assertEquals("com.vmenon.mpo.login.test", appContext.packageName)
     }
 }
