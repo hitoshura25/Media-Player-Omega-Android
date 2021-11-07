e9bbaae13884142c289b1f359ee0c59795548261
Vinayak Menon <vviswana@gmail.com>
2021-11-06 21:54:42 -0500

    Adding new presentation modules

---
+++ login_presentation/src/main/java/com/vmenon/mpo/login_presentation/di/dagger/LoginModule.kt
@@ -0,0 +1,8 @@
+package com.vmenon.mpo.login_presentation.di.dagger
+
+import dagger.Module
+
+@Module
+object LoginModule {
+
+}
---
+++ login_presentation/src/main/java/com/vmenon/mpo/login_presentation/di/dagger/LoginScope.kt
@@ -0,0 +1,7 @@
+package com.vmenon.mpo.login_presentation.di.dagger
+
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class LoginScope
