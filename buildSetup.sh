cafbd6bd025fba74ab6075c19e928a296a5859f9
Vinayak Menon <vviswana@gmail.com>
2021-11-20 21:59:05 -0600

    Adding some new workflows to Circle CI and renaming some things in the Fastfile

--- buildSetup.sh
+++ buildSetup.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
-function checkVar() {
-  if [[ -z "${!1}" ]]; then
-    echo "$1 is undefined"
-  exit 1
-  fi
-}
-
-function safeRunCommand() {
-  typeset command="$*"
-  typeset ret_code
-
-  echo command="$command"
-  eval "$command"
-  ret_code=$?
-  if [ $ret_code != 0 ]; then
-    printf "Error : [%d] when executing command: '$command'" $ret_code
-    exit $ret_code
-  fi
-}
-
 set -e
-export BUNDLE_LOCATION="./app/build/outputs/bundle/release/app-release.aab"
+source ./buildEnv.sh
 checkVar "SERVICE_ACCOUNT_FILE"
 checkVar "android_keystore_password"
 checkVar "android_keystore_key_alias"
@@ -44,4 +24,3 @@ fi
 
 safeRunCommand "gpg --passphrase $MPO_ENCRYPTION_KEY --pinentry-mode loopback -o $android_keystore_file -d $android_keystore_file.gpg"
 safeRunCommand "gpg --passphrase $MPO_ENCRYPTION_KEY --pinentry-mode loopback -o $SERVICE_ACCOUNT_FILE -d $SERVICE_ACCOUNT_FILE.gpg"
-safeRunCommand "bundle exec fastlane deploy_internal_share"
