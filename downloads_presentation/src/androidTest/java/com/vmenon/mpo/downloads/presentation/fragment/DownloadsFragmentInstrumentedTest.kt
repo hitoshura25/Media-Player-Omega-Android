826e96c371d2b35c35212449581e96a2b8c91635
Vinayak Menon <vviswana@gmail.com>
2021-12-04 19:01:55 -0600

    Reworking the build commands to see if instrumention tests will work

--- downloads_presentation/src/androidTest/java/com/vmenon/mpo/downloads/presentation/fragment/DownloadsFragmentInstrumentedTest.kt
+++ downloads_presentation/src/androidTest/java/com/vmenon/mpo/downloads/presentation/fragment/DownloadsFragmentInstrumentedTest.kt
@@ -5,7 +5,7 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.vmenon.mpo.downloads.R
+import com.vmenon.mpo.downloads.presentation.test.R
 import org.hamcrest.core.AllOf.allOf
 import org.junit.Test
 import org.junit.runner.RunWith
--- runInstrumentationTests.sh
+++ runInstrumentationTests.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 source ./buildEnv.sh
 set -e
+safeRunCommand "./gradlew app:deployBundleApksDebug app:installDebugAndroidTest"
 safeRunCommand "adb shell am instrument -e clearPackageData \"true\" -e tags \"@smoke\" -w com.vmenon.mpo.test/com.vmenon.mpo.test.CucumberTestRunner"
+#safeRunCommand "./gradlew connectedAndroidTest"
