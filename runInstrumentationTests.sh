8a5a061e524aefb054fe30b96b63dd7fa7ccbfbf
Vinayak Menon <vviswana@gmail.com>
2021-12-11 15:06:17 -0600

    Adding "./" to path for android-wait-for-emulator script

--- runInstrumentationTests.sh
+++ runInstrumentationTests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ./buildEnv.sh
 set -e
-safeRunCommand "android-wait-for-emulator"
+safeRunCommand "./android-wait-for-emulator"
 safeRunCommand "./gradlew app:deployBundleApksDebug app:installDebugAndroidTest"
 safeRunCommand "adb shell am instrument -e clearPackageData \"true\" -e tags \"@smoke\" -w com.vmenon.mpo.test/com.vmenon.mpo.test.CucumberTestRunner"
 safeRunCommand "./gradlew connectedAndroidTest"
