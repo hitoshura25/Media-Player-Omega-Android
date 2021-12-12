c8223733898760be5aa72de12c3dc9fa32fd7bce
Vinayak Menon <vviswana@gmail.com>
2021-12-12 17:33:16 -0600

    Adding logs to archive for more debugging test failures

--- runInstrumentationTests.sh
+++ runInstrumentationTests.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+
+function exitTestRun() {
+  safeRunCommand "adb pull /sdcard/Pictures/screenshots /tmp/android_test/screenshots"
+  safeRunCommand "adb logcat -d > /tmp/android_test/logcat.log"
+}
+
+trap exitTestRun EXIT
 source ./buildEnv.sh
 set -e
+safeRunCommand "rm -rf /tmp/android_test || true"
+safeRunCommand "mkdir -p /tmp/android_test"
 safeRunCommand "./android-wait-for-emulator"
 safeRunCommand "./gradlew app:deployBundleApksDebug app:installDebugAndroidTest"
-safeRunCommand "adb shell am instrument -e listener \"com.vmenon.mpo.test.TestListener\" -e clearPackageData \"true\" -e tags \"@smoke\" -w com.vmenon.mpo.test/com.vmenon.mpo.test.CucumberTestRunner"
+safeRunCommand "adb shell am instrument -e listener \"com.vmenon.mpo.test.TestListener\" -e clearPackageData \"true\" -e tags \"@smoke\" -w com.vmenon.mpo.test/com.vmenon.mpo.test.CucumberTestRunner | tee -a /tmp/android_test/adb-test.log"
+
+# adb doesn't propagate exit code from tests, see https://code.google.com/p/android/issues/detail?id=3254
+# So we need to parse saved terminal log
+safeRunCommand "cat /tmp/android_test/adb-test.log | grep \"OK (\""
 #safeRunCommand "./gradlew connectedAndroidTest"
-safeRunCommand "adb pull /sdcard/Pictures/screenshots /tmp/android_test_screenshots"
