75280bfcf938da53c0336ea2559df37d4deedec5
Vinayak Menon <vviswana@gmail.com>
2021-12-15 20:55:15 -0600

    Logging the path to the screenshots

--- app/src/androidTest/java/com/vmenon/mpo/test/CustomScreenCaptureProcessor.kt
+++ app/src/androidTest/java/com/vmenon/mpo/test/CustomScreenCaptureProcessor.kt
@@ -1,6 +1,7 @@
 package com.vmenon.mpo.test
 
 import android.os.Environment
+import android.util.Log
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.runner.screenshot.BasicScreenCaptureProcessor
 import java.io.File
@@ -9,6 +10,7 @@ class CustomScreenCaptureProcessor : BasicScreenCaptureProcessor() {
     init {
         mTag = "CustomScreenCaptureProcessor"
         mDefaultScreenshotPath = getNewFilename()
+        Log.i(mTag, "Screenshot path: ${mDefaultScreenshotPath.absolutePath}")
     }
 
     private fun getNewFilename(): File? {
