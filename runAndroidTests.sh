5e38967552d0d303f9342a22a75c9d7b64909865
Vinayak Menon <vviswana@gmail.com>
2021-12-18 18:52:49 -0600

    Adding screenrecord

--- runAndroidTests.sh
+++ runAndroidTests.sh
@@ -2,7 +2,9 @@
 
 function exitTestRun() {
   safeRunCommand "adb pull /sdcard/Android/data/com.vmenon.mpo/files/Pictures/test_run_screenshots /tmp/android_test/screenshots || true"
+  safeRunCommand "adb pull /sdcard/cucumber_tests_screenrecord.mp4 /tmp/android_test/cucumber_tests_screenrecord.mp4 || true"
   safeRunCommand "adb logcat -d > /tmp/android_test/logcat.log || true"
+  safeRunCommand "kill 0 || true"
 }
 
 trap exitTestRun EXIT
@@ -12,8 +14,12 @@ safeRunCommand "rm -rf /tmp/android_test || true"
 safeRunCommand "mkdir -p /tmp/android_test"
 safeRunCommand "./android-wait-for-emulator"
 safeRunCommand "./gradlew app:deployBundleApksDebug app:installDebugAndroidTest"
+safeRunCommand "adb shell screenrecord /sdcard/cucumber_tests_screenrecord.mp4 &"
+SCREENRECORD_PID=$!
 safeRunCommand "adb shell am instrument -e listener \"com.vmenon.mpo.test.TestListener\" -e clearPackageData \"true\" -e tags \"@smoke\" -w com.vmenon.mpo.test/com.vmenon.mpo.test.CucumberTestRunner | tee -a /tmp/android_test/adb-test.log"
-
+safeRunCommand "kill $SCREENRECORD_PID"
+# Wait for 3 seconds for the device to compile the video
+safeRunCommand "sleep 3"
 # adb doesn't propagate exit code from tests, see https://code.google.com/p/android/issues/detail?id=3254
 # So we need to parse saved terminal log
 safeRunCommand "cat /tmp/android_test/adb-test.log | grep \"OK (\""
