8b0f573dfc5dd03e4789e7e2fe637eabcd980163
Vinayak Menon <vviswana@gmail.com>
2021-12-30 16:29:50 -0600

    Somehow didn't commit a file...

--- runCucumberTests.sh
+++ runCucumberTests.sh
@@ -11,9 +11,6 @@ source ./buildEnv.sh
 set -e
 safeRunCommand "rm -rf /tmp/android_test || true"
 safeRunCommand "mkdir -p /tmp/android_test"
-safeRunCommand "./android-wait-for-emulator"
-safeRunCommand "adb uninstall com.vmenon.mpo || true"
-safeRunCommand "./gradlew app:deployBundleApksDebug app:installDebugAndroidTest"
 safeRunCommand "adb shell screenrecord /sdcard/cucumber_tests_screenrecord.mp4 &"
 SCREENRECORD_PID=$!
 safeRunCommand "adb shell am instrument -e listener \"com.vmenon.mpo.test.TestListener\" -e coverage \"true\" -e coverageFile \"/sdcard/Android/data/com.vmenon.mpo/files/coverage.ec\" -e clearPackageData \"true\" -e tags \"@smoke\" -w com.vmenon.mpo.test/com.vmenon.mpo.test.CucumberTestRunner | tee -a /tmp/android_test/adb-test.log"
@@ -23,6 +20,5 @@ safeRunCommand "sleep 3"
 # adb doesn't propagate exit code from tests, see https://code.google.com/p/android/issues/detail?id=3254
 # So we need to parse saved terminal log
 safeRunCommand "cat /tmp/android_test/adb-test.log | grep \"OK (\""
-safeRunCommand "./gradlew connectedAndroidTest"
 safeRunCommand "mkdir -p ./app/build/outputs/code_coverage/debugAndroidTest/connected"
 safeRunCommand "adb pull /sdcard/Android/data/com.vmenon.mpo/files/coverage.ec ./app/build/outputs/code_coverage/debugAndroidTest/connected"
