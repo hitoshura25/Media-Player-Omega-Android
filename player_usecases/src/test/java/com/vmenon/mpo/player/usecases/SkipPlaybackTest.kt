77d7bb0ffcafc9ccc792d3baa6e36664eaabb07d
Vinayak Menon <vviswana@gmail.com>
2022-01-15 14:09:58 -0600

    Adding test for SkipPlayback usecase

---
+++ player_usecases/src/test/java/com/vmenon/mpo/player/usecases/SkipPlaybackTest.kt
@@ -0,0 +1,72 @@
+package com.vmenon.mpo.player.usecases
+
+import com.vmenon.mpo.player.domain.MediaPlayerEngine
+import com.vmenon.mpo.player.domain.PlaybackState
+import com.vmenon.mpo.player.usecases.SkipPlayback.Companion.MEDIA_SKIP_GRACE_PERIOD_MS
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Test
+import org.mockito.kotlin.*
+
+@ExperimentalCoroutinesApi
+class SkipPlaybackTest {
+    private val playerEngine: MediaPlayerEngine = mock()
+
+    @Test
+    fun skipPlaybackDoesNothingIfPlaybackStateIsNull() = runBlockingTest {
+        val usecase = SkipPlayback(playerEngine)
+        usecase.invoke(100L)
+        verify(playerEngine, times(0)).seekTo(any())
+    }
+
+    @Test
+    fun skipPlaybackDoesNothingIfPlaybackStateIsNotValid() = runBlockingTest {
+        val usecase = SkipPlayback(playerEngine)
+        val playbackState = TestData.playbackState.copy(state = PlaybackState.State.NONE)
+        whenever(playerEngine.getCurrentPlaybackState()).thenReturn(playbackState)
+        usecase.invoke(100L)
+        verify(playerEngine, times(0)).seekTo(any())
+    }
+
+    @Test
+    fun skipPlaybackIfCurrentlyPlayingAndWithinGracePeriod() = runBlockingTest {
+        val usecase = SkipPlayback(playerEngine)
+        val playbackState = TestData.playbackState.copy(state = PlaybackState.State.PLAYING)
+        whenever(playerEngine.getCurrentPlaybackState()).thenReturn(playbackState)
+        usecase.invoke(100L)
+        verify(playerEngine).seekTo(100L)
+    }
+
+    @Test
+    fun skipPlaybackSeeksToEndWithGracePeriodIfCurrentlyPlayingWithinGracePeriod() = runBlockingTest {
+        val usecase = SkipPlayback(playerEngine)
+        val playbackState = TestData.playbackState.copy(
+            state = PlaybackState.State.PLAYING,
+            positionInMillis = TestData.playbackMedia.durationInMillis - MEDIA_SKIP_GRACE_PERIOD_MS
+        )
+        whenever(playerEngine.getCurrentPlaybackState()).thenReturn(playbackState)
+        usecase.invoke((MEDIA_SKIP_GRACE_PERIOD_MS + 1).toLong())
+        verify(playerEngine).seekTo(any())
+    }
+
+    @Test
+    fun skipPlaybackDoesNothingIfCurrentlyPlayingAndNotWithinGracePeriod() = runBlockingTest {
+        val usecase = SkipPlayback(playerEngine)
+        val playbackState = TestData.playbackState.copy(
+            state = PlaybackState.State.PLAYING,
+            positionInMillis = (TestData.playbackMedia.durationInMillis - MEDIA_SKIP_GRACE_PERIOD_MS) + 1
+        )
+        whenever(playerEngine.getCurrentPlaybackState()).thenReturn(playbackState)
+        usecase.invoke((MEDIA_SKIP_GRACE_PERIOD_MS + 1).toLong())
+        verify(playerEngine, times(0)).seekTo(any())
+    }
+
+    @Test
+    fun skipPlaybackSeeksToStartIfAmountTooLarge() = runBlockingTest {
+        val usecase = SkipPlayback(playerEngine)
+        val playbackState = TestData.playbackState.copy(state = PlaybackState.State.PLAYING)
+        whenever(playerEngine.getCurrentPlaybackState()).thenReturn(playbackState)
+        usecase.invoke(-100L)
+        verify(playerEngine).seekTo(0L)
+    }
+}
--- player_usecases/src/test/java/com/vmenon/mpo/player/usecases/TestData.kt
+++ player_usecases/src/test/java/com/vmenon/mpo/player/usecases/TestData.kt
@@ -2,13 +2,21 @@ package com.vmenon.mpo.player.usecases
 
 import com.vmenon.mpo.player.domain.PlaybackMedia
 import com.vmenon.mpo.player.domain.PlaybackMediaRequest
+import com.vmenon.mpo.player.domain.PlaybackState
 
 object TestData {
+    val playbackMedia = PlaybackMedia(
+        mediaId = "mediaId",
+        durationInMillis = 120000L
+    )
     val playbackMediaRequest = PlaybackMediaRequest(
-        media = PlaybackMedia(
-            mediaId = "mediaId",
-            durationInMillis = 10L
-        ),
+        media = playbackMedia,
         mediaFile = "file"
+    )
+    val playbackState = PlaybackState(
+        media = playbackMedia,
+        positionInMillis = 0L,
+        state = PlaybackState.State.NONE,
+        playbackSpeed = 1F
     )
 }
