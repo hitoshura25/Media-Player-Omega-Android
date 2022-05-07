7c2d6c44cd3d4a5f1e8159fd428ebb34b7130411
Vinayak Menon <vviswana@gmail.com>
2022-05-06 21:32:40 -0500

    Honestly, can't remember why I did any of this...I think to make manage Dagger DI for the tests easier?

--- common_framework/src/main/java/com/vmenon/mpo/common/framework/di/dagger/DaggerComponentProviders.kt
+++ common_framework/src/main/java/com/vmenon/mpo/common/framework/di/dagger/DaggerComponentProviders.kt
@@ -3,6 +3,7 @@ package com.vmenon.mpo.common.framework.di.dagger
 import com.vmenon.mpo.auth.framework.di.dagger.AuthComponent
 import com.vmenon.mpo.auth.framework.di.dagger.AuthComponentProvider
 import com.vmenon.mpo.auth.framework.di.dagger.BiometricsComponent
+import com.vmenon.mpo.auth.framework.di.dagger.DaggerAuthComponent
 import com.vmenon.mpo.navigation.framework.di.dagger.NavigationFrameworkComponent
 import com.vmenon.mpo.persistence.di.dagger.PersistenceComponent
 import com.vmenon.mpo.system.framework.di.dagger.SystemFrameworkComponent
@@ -10,23 +11,25 @@ import com.vmenon.mpo.system.framework.di.dagger.SystemFrameworkComponentProvide
 
 class DaggerComponentProviders(
     val systemFrameworkComponent: SystemFrameworkComponent,
-    val navigationFrameworkComponent: NavigationFrameworkComponent,
-    val persistenceComponent: PersistenceComponent,
-    val biometricsComponent: BiometricsComponent,
+    biometricsComponent: BiometricsComponent,
+    navigationFrameworkComponent: NavigationFrameworkComponent,
+    persistenceComponent: PersistenceComponent,
     apiUrl: String,
 ) : SystemFrameworkComponentProvider, AuthComponentProvider, CommonFrameworkComponentProvider {
 
-    private val authComponentProvider =
-        DaggerAuthComponentProvider(systemFrameworkComponent, biometricsComponent)
+    private val authComponent = DaggerAuthComponent.builder()
+        .systemFrameworkComponent(systemFrameworkComponent)
+        .biometricsComponent(biometricsComponent)
+        .build()
     private val commonFrameworkComponentProvider = DaggerCommonFrameworkComponentProvider(
         systemFrameworkComponent,
-        authComponentProvider.authComponent(),
+        authComponent,
         persistenceComponent,
         navigationFrameworkComponent,
         apiUrl
     )
 
-    override fun authComponent(): AuthComponent = authComponentProvider.authComponent()
+    override fun authComponent(): AuthComponent = authComponent
 
     override fun commonFrameworkComponent(): CommonFrameworkComponent =
         commonFrameworkComponentProvider.commonFrameworkComponent()
